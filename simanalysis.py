02a989caf6f014b32a7a84e81cb7cf7ec64aedad
firmcode <pig6775@naver.com>
2019-08-16 02:36:11 +0900

    Update simanalysis.py
    complete tokenize_petition and insert

--- simanalysis.py
+++ simanalysis.py
@@ -3,6 +3,7 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import sys
 from konlpy.tag import Kkma
 from konlpy.utils import pprint
+from konlpy.tag import Twitter
 '''
 CREATE TABLE simanalysis( id char(10) NOT NULL, token text(65535), sim1_id char(10), sim2_id char(10), sim3_id char(10), tokenizing_status int, PRIMARY KEY(id));
 tokenizing_status 0 : progress and *
@@ -10,16 +11,49 @@ tokenizing_status 1 : expired and non-complete tokenize
 tokenizing_status 2 : expired and complete tokenize
 '''
 
-def insert_token(petition_id, token_content,tokenizing_status): 
-	try:
-		sql = """INSERT INTO simanalysis (id, token, tokenizing_status) VALUES (%s, %s, %s)"""
+def update_token(petition_id, token_content,tokenizing_status): 
+	print("update")
+	sys.exit()
+	#try:
+	sql = """INSERT INTO simanalysis (id, token, tokenizing_status) VALUES (%s, %s, %s)"""
 
-		curs.execute(sql, (petition_id, token_content, tokenizing_status))
-		conn.commit()
+	print(petition_id,token_content,tokenizing_status)
+	token_content_str=' '.join(token_content).strip()
+	print(token_content_str)
+	curs.execute(sql, (petition_id, token_content, tokenizing_status))
+	conn.commit()
 
-	except:
-	    print("# insert except!!")
-	    sys.exit() 
+	#except:
+	#    print("# insert except!!")
+	#    sys.exit() 
+
+
+def insert_token(petition_id, petition_content, tokenizing_status): 
+	sql = "SELECT status FROM petition_crawled WHERE id = %s" # get crawled status
+	curs.execute(sql, petition_id)
+	crawled_status = curs.fetchone()
+
+	if crawled_status != None: # if item already exists
+		tokenizing_status = crawled_status[0] # progress -> progress
+	else :
+		print("Error!! non crawled_status of petition_crawled")
+		sys.exit()
+
+	twitter = Twitter()#for tokenizing made by twitter
+	token_content = twitter.morphs(petition_content)
+
+
+	#try:
+	sql = """INSERT INTO simanalysis (id, token, tokenizing_status) VALUES (%s, %s, %s)"""
+
+	token_content_str=' '.join(token_content).strip()
+	print(token_content_str,tokenizing_status)
+	#curs.execute(sql, (petition_id, token_content_str, tokenizing_status))
+	#conn.commit()
+
+	#except:
+	#    print("# insert except!!")
+	#    sys.exit() 
 
 
 def tokenize_petition(petition_id,petition_content):
@@ -27,49 +61,28 @@ def tokenize_petition(petition_id,petition_content):
 	curs.execute(sql, petition_id)
 	tokenizing_status = curs.fetchone()
 
-	print("tokenizing_status",tokenizing_status)
-
-	if tokenizing_status != None:#update tokenizing_status
-		if (tokenizing_status == 0): 
-			tokenizing_status = 0 # progress -> progress
-		elif (tokenizing_status == 1): 
-			tokenizing_status = 2 # expired -> expired and complete tokenize
-		elif (tokenizing_status == 2): 
-			return 1	#completed tokenize ,so nomore tokenizing
-		else:
-			print("Error! tokenizing_status")
-			sys.exit()
+	if tokenizing_status != None :
+		if tokenizing_status[0]==0:
+			update_token(petition_id, token_content,tokenizing_status)
+		elif tokenizing_status[1]==0:
+			return 1 #completed tokenize ,so nomore tokenizing
 	else :
-		sql = "SELECT status FROM petition_crawled WHERE id = %s"
-		curs.execute(sql, petition_id)
-		status = curs.fetchone()
-
-		if status != None: # if item already exists
-			if status[0] == '0':
-				tokenizing_status = 0 # progress -> progress
-			elif status[0] == '1': 
-				tokenizing_status = 2 # expired -> expired and complete tokenize
-			
-
-	kkma = Kkma()#for konlpy
-	token_content = kkma.nouns(petition_content)
-	insert_token(petition_id, token_content,tokenizing_status)
-
-
+		insert_token(petition_id, petition_content, tokenizing_status)
 
 def get_crawled_db():
-	sql = "SELECT id,content FROM petition_crawled"
+	#sql = "SELECT id, content FROM petition_crawled"
+	sql = "SELECT id, content FROM petition_crawled limit 100"
 	curs.execute(sql)
 	
 	content_cnt = curs.rowcount
 	print("rowcount : ", curs.rowcount)
 	rows = curs.fetchall() # get all DB
-	'''
+
 	for row in rows:
 		petition_id = row[0]
 		petition_content = row[1]
 		tokenize_petition(petition_id,petition_content)
-	'''
+
 	return 0
 
 
@@ -92,7 +105,7 @@ def petition_content2vec():
 
 
 if __name__ == "__main__":
-	conn = pymysql.connect(host='127.0.0.1', port=3306, user='modutrend', password='trend1q2w3e!@',db='petition', charset='utf8mb4', )
+	conn = pymysql.connect(host='35.200.82.12', port=3306, user='modutrend', password='trend1q2w3e!@',db='petition', charset='utf8mb4', )
 	curs = conn.cursor()
 
 	print("connect success")
@@ -101,4 +114,3 @@ if __name__ == "__main__":
 
 
 	#petition_content2vec()
-
